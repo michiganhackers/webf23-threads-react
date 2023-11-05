3b9894b19bf51a786c06085eda954bd287f8b43d
Brian Travis <bztravis88@gmail.com>
2023-11-04 21:34:15 -0400

    Add ThreadInput input form

--- src/components/ThreadInput.js
+++ src/components/ThreadInput.js
@@ -1,5 +1,12 @@
 import './ThreadInput.css'
 
 export default function ThreadInput() {
-  return <div className="threadInputContainer">ThreadInput</div>
+  return (
+    <div className="threadInputContainer">
+      <form>
+        <input type="text" placeholder="Start a thread..." name="thread" />
+        <button type="submit">Post</button>
+      </form>
+    </div>
+  )
 }
