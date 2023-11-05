32019219b35b7e0d065712c85707a29a24a4aee6
Brian Travis <bztravis88@gmail.com>
2023-11-04 21:17:38 -0400

    Add basic file structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import ThreadInput from './components/ThreadInput'
+import Thread from './components/Thread'
+import './App.css'
+
 function App() {
-  return <div className="container">hello world</div>
+  return (
+    <div className="container">
+      <img src="/logo.png" alt="logo" />
+      <ThreadInput />
+      <Thread />
+    </div>
+  )
 }
 
 export default App
---
+++ src/components/ThreadInput.js
@@ -0,0 +1,3 @@
+export default function ThreadInput() {
+  return <div>ThreadInput</div>
+}
